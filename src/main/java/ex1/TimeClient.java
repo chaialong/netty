d1b918d713e63a341ae1462707a634e9c1fab089
Chai Along <chaialong@xiaomi.com>
2017-08-21 22:16:31 +0800

    Sync blocking IO exercise
    N/A
    
    Signed-off-by: Chai Along <chaialong@xiaomi.com>

---
+++ src/main/java/ex1/TimeClient.java
@@ -0,0 +1,52 @@
+package ex1;
+
+import org.apache.commons.io.IOUtils;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+/**
+ * Created by mi on 17-8-21.
+ */
+public class TimeClient {
+    public static void main(String[] args) throws InterruptedException {
+        for (int i = 0; i < 10; i++) {
+            Thread.currentThread().sleep(1000);
+            queryTime();
+        }
+    }
+
+    private static void queryTime() {
+        int port = 8080;
+
+        Socket socket = null;
+        BufferedReader in = null;
+        PrintWriter out = null;
+
+        try {
+            socket = new Socket("127.0.0.1", port);
+            in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            out = new PrintWriter(socket.getOutputStream(), true);
+            out.println("QUERY TIME ORDER");
+            System.out.println("Send order 2 server succeed.");
+            String resp = in.readLine();
+            System.out.println("Now is : " + resp);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            IOUtils.closeQuietly(in);
+            IOUtils.closeQuietly(out);
+            if (socket != null) {
+                try {
+                    socket.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                socket = null;
+            }
+        }
+    }
+}
